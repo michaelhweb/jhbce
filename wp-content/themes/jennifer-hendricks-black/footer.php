cbc068042f852b77d34cbc51999051b0e6274442
michaelhweb <michael@michaelheple.com>
2015-05-25 12:13:23 -0700

    moved to dev
    moved the site to development

--- wp-content/themes/jennifer-hendricks-black/footer.php
+++ wp-content/themes/jennifer-hendricks-black/footer.php
@@ -7,28 +7,33 @@
 ?>
 
 			</div><!--.site-content-->
-			
-			
-			<footer id="site-footer" role="contentinfo">
-				<div id="footer-row" class="row site-footer">
-					<div class="col-md-4 footer-left">
-						<?php 
-						if (!dynamic_sidebar('footer-left')) {
-							printf(__('Powered by %s', 'bootstrap-basic'), 'WordPress');
-							/**echo ' | ';
-							printf(__('Theme: %s', 'bootstrap-basic'), '<a href="http://okvee.net">Bootstrap Basic</a>');*/
-						} 
-						?> 
-					</div>
-                    <div class="col-md-4 footer-right text-right">
-						<?php dynamic_sidebar('footer-middle'); ?> 
-					</div>
-					<div class="col-md-4 footer-right text-right">
-						<?php dynamic_sidebar('footer-right'); ?> 
-					</div>
-				</div>
-			</footer>
 		</div><!--.container page-container-->
+			
+			
+        <footer class="containter-fluid" id="site-footer" role="contentinfo">
+            <div class="container footer-container">
+                <div id="footer-row" class="row site-footer">
+                    <div class="col-md-4 footer-left">
+                        <?php 
+                        if (!dynamic_sidebar('footer-left')) {
+                            printf(__('Powered by %s', 'bootstrap-basic'), 'WordPress');
+                            /**echo ' | ';
+                            printf(__('Theme: %s', 'bootstrap-basic'), '<a href="http://okvee.net">Bootstrap Basic</a>');*/
+                        } 
+                        ?> 
+                    </div>
+                    <div class="col-md-4 footer-right text-right">
+                        <?php dynamic_sidebar('footer-middle'); ?> 
+                    </div>
+                    <div class="col-md-4 footer-right text-right">
+                        <?php dynamic_sidebar('footer-right'); ?> 
+
+                    </div>
+                    <!--Copyright-->
+                    <span id="footer-cw">All Content Copyright &copy; 2015<br>Jennifer Hendricks Black Ceramic Arts</span>
+                </div>
+            </div>
+        </footer>
 		
 		
 		<!--wordpress footer-->
--- wp-content/themes/jennifer-hendricks-black/index.php
+++ wp-content/themes/jennifer-hendricks-black/index.php
@@ -15,7 +15,8 @@ $main_column_size = bootstrapBasicGetMainColumnSize();
 <?php get_sidebar('left'); ?> 
 				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
 					<main id="main" class="site-main" role="main">
-						<?php if (have_posts()) { ?> 
+						<?php if (is_home()) {query_posts("cat=5");}?>
+                        <?php if (have_posts()) { ?> 
 						<?php 
 						// start the loop
 						while (have_posts()) {
